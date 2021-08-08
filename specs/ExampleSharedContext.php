5f49d5a51ae638c0a66876fd30c81e52fd3fafac
Joe Forshaw <joe@joeforshaw.com>
2021-08-08 12:46:04 +0100

    The speckl runner will look for *SharedContext.php files

---
+++ specs/ExampleSharedContext.php
@@ -0,0 +1,5 @@
+<?php
+
+sharedContext('shared context from another file', function() {
+  $this->sharedContextVariable = 'My shared context variable from another file';
+});
--- specs/SharedContextSpec.php
+++ specs/SharedContextSpec.php
@@ -1,13 +1,23 @@
 <?php
 
 describe('a sharedContext block', function() {
-  includeContext('example sharedContext');
+  context('when defined in the same file', function() {
+    includeContext('shared context from the same file');
 
-  it('allows variables to be shared', function() {
-    expect($this->sharedContextVariable)->to->equal('My shared context variable');
+    it('allows variables to be shared between blocks', function() {
+      expect($this->sharedContextVariable)->to->equal('My shared context variable in the same file');
+    });
+  });
+
+  context('when defined in another file', function() {
+    includeContext('shared context from another file');
+
+    it('allows variables to be shared between blocks', function() {
+      expect($this->sharedContextVariable)->to->equal('My shared context variable from another file');
+    });
   });
 });
 
-sharedContext('example sharedContext', function() {
-  $this->sharedContextVariable = 'My shared context variable';
+sharedContext('shared context from the same file', function() {
+  $this->sharedContextVariable = 'My shared context variable in the same file';
 });
