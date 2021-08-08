e4461cb05a16b9cf155ddade207a0c5a8244e2a7
Joe Forshaw <joe@joeforshaw.com>
2021-08-08 10:07:52 +0100

    Added another xit spec

--- specs/PendingSpec.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-describe('xit function', function() {
-  xit('test this text is yellow', function() {
-    expect(true)->to->beFalse(); // This shouldn't run
-  });
-});
---
+++ specs/XitSpec.php
@@ -0,0 +1,11 @@
+<?php
+
+describe('an xit block', function() {
+  xit('renders the label yellow', function() {
+    expect(true)->to->beFalse(); // This shouldn't run
+  });
+
+  xit('adds (pending) to the end of the label', function() {
+    expect(true)->to->beFalse(); // This shouldn't run
+  });
+});
