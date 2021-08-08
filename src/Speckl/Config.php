5f088e6f10ed570dfcf4253fdab6ed662c732fb4
Joe Forshaw <joe@joeforshaw.com>
2021-08-08 10:08:04 +0100

    Fixed indenting

--- src/Speckl/Config.php
+++ src/Speckl/Config.php
@@ -5,14 +5,14 @@ namespace Speckl;
 class Config {
   public static function get($key) {
     if (!array_key_exists('speckl', $GLOBALS)) {
-        $GLOBALS['speckl'] = [];
+      $GLOBALS['speckl'] = [];
     }
     return $GLOBALS['speckl'][$key];
   }
 
   public static function set($key, $value) {
     if (!array_key_exists('speckl', $GLOBALS)) {
-        $GLOBALS['speckl'] = [];
+      $GLOBALS['speckl'] = [];
     }
     $GLOBALS['speckl'][$key] = $value;
   }
