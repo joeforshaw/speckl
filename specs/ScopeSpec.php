fe2b536edf0a3f04577c88e98ba73ae4871039d7
Joe Forshaw <joe@joeforshaw.com>
2021-08-08 14:18:04 +0100

    Added scope spec

--- specs/ScopeSpec.php
+++ specs/ScopeSpec.php
@@ -4,8 +4,16 @@ describe('Scope', function() {
   $this->exampleCallableProperty = function() {
     return 'Callable property output';
   };
+  $this->propertyInScope = 'This a property defined within this scope';
+  $this->boundExampleCallableProperty = function() {
+    return $this->propertyInScope;
+  };
 
   it('can run callable properties defined in scope', function() {
     expect($this->exampleCallableProperty())->to->equal('Callable property output');
+  });
+
+  it('binds itself to callable properties', function() {
+    expect($this->boundExampleCallableProperty())->to->equal('This a property defined within this scope');
   });
 });
