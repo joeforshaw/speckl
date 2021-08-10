aba3536a82fb29bc9761dab1c6b122c35ac8d413
Joe Forshaw <joe@joeforshaw.com>
2021-08-10 09:53:22 +0100

    Moved tests around

--- specs/ScopeSpec.php
+++ specs/ScopeSpec.php
@@ -16,12 +16,12 @@ describe('Scope', function() {
   context('when an existing scope variable is redefined within a child scope', function() {
     $this->propertyInScope = 'This is a property redefined within a child scope';
 
-    it('reflects the new value', function() {
+    it('reflects the value set in the child scope', function() {
       expect($this->boundExampleCallableProperty())->to->equal('This is a property redefined within a child scope');
     });
 
     context('when an existing scope variable is redefined within an example', function() {
-      it('reflects the new value', function() {
+      it('reflects the value set in the example', function() {
         $this->propertyInScope = 'This is a property redefined within an example';
         expect($this->boundExampleCallableProperty())->to->equal('This is a property redefined within an example');
       });
