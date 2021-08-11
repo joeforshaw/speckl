5bda070e01fbda15bb9d625a050bf0bc0058e957
Joe Forshaw <joe@joeforshaw.com>
2021-08-11 11:34:24 +0100

    Added implicit subject test case for TestClassWithConstructorWithParameters

--- specs/ImplicitSubjectSpec.php
+++ specs/ImplicitSubjectSpec.php
@@ -6,6 +6,10 @@ class TestClassWithParameterlessConstructor {
   public function __construct() {}
 }
 
+class TestClassWithConstructorWithParameters {
+  public function __construct($parameter) {}
+}
+
 describe(TestClassWithNoConstructor::class, function() {
   it('initializes an implicit subject', function() {
     expect($this->subject)->toBe->AnInstanceOf(TestClassWithNoConstructor::class);
@@ -15,5 +19,11 @@ describe(TestClassWithNoConstructor::class, function() {
 describe(TestClassWithParameterlessConstructor::class, function() {
   it('initializes an implicit subject', function() {
     expect($this->subject)->toBe->AnInstanceOf(TestClassWithParameterlessConstructor::class);
+  });
+});
+
+describe(TestClassWithConstructorWithParameters::class, function() {
+  it("doesn't initialize an implicit subject", function() {
+    expect($this->subject)->to->beNull();
   });
 });
