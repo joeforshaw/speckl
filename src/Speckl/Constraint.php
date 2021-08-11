f6405354404f07f1ee6e1a7ff2f3b744d5109df0
Joe Forshaw <joe@joeforshaw.com>
2021-08-11 09:49:39 +0100

    Added expected string to all constraints

--- src/Speckl/Constraint.php
+++ src/Speckl/Constraint.php
@@ -11,67 +11,67 @@ class Constraint {
     $this->negated = $negated;
   }
 
-  public function true() { $this->check($this->actual === true); }
+  public function true() { $this->check($this->actual === true, true); }
   public function beTrue() { $this->true(); }
 
-  public function false() { $this->check($this->actual === false); }
+  public function false() { $this->check($this->actual === false, false); }
   public function beFalse() { $this->false(); }
 
-  public function null() { $this->check(is_null($this->actual)); }
+  public function null() { $this->check(is_null($this->actual), null); }
   public function beNull() { $this->null(); }
 
-  public function string() { $this->check(is_string($this->actual)); }
+  public function string() { $this->check(is_string($this->actual), 'string'); }
   public function beString() { $this->string(); }
 
-  public function beEmpty() { $this->check(empty($this->actual)); }
+  public function beEmpty() { $this->check(empty($this->actual), 'empty'); }
 
-  public function theSameSizeAs($exp) { $this->check(count($this->actual) === count($exp)); }
+  public function theSameSizeAs($exp) { $this->check(count($this->actual) === count($exp), 'length of ' . count($exp)); }
   public function beTheSameSizeAs($exp) { $this->theSameSizeAs($exp); }
 
-  public function AnInstanceOf($class) { $this->check($this->actual instanceof $class); }
+  public function AnInstanceOf($class) { $this->check($this->actual instanceof $class, $class); }
   public function beAnInstanceOf($class) { $this->AnInstanceOf($class); }
 
   public function equal($exp) { $this->check($this->actual === $exp, $exp); }
   public function equalTo($exp) { $this->equal($exp); }
   public function eq($exp) { $this->equal($exp); }
 
-  public function greaterThan($exp) { $this->check($this->actual > $exp, $exp); }
+  public function greaterThan($exp) { $this->check($this->actual > $exp, "greater than $exp"); }
   public function beGreaterThan($exp) { $this->greaterThan($exp); }
 
-  public function greaterThanOrEqualTo($exp) { $this->check($this->actual >= $exp, $exp); }
+  public function greaterThanOrEqualTo($exp) { $this->check($this->actual >= $exp, "greater than or equal to $exp"); }
   public function beGreaterThanOrEqualTo($exp) { $this->greaterThanOrEqualTo($exp); }
   public function gte($exp) { $this->greaterThanOrEqualTo($exp); }
 
-  public function lessThan($exp) { $this->check($this->actual < $exp, $exp); }
+  public function lessThan($exp) { $this->check($this->actual < $exp, "less than $exp"); }
   public function beLessThan($exp) { $this->lessThan($exp); }
 
-  public function lessThanOrEqualTo($exp) { $this->check($this->actual <= $exp, $exp); }
+  public function lessThanOrEqualTo($exp) { $this->check($this->actual <= $exp, "less than or equal to $exp"); }
   public function belessThanOrEqualTo($exp) { $this->lessThanOrEqualTo($exp); }
   public function lte($exp) { $this->lessThanOrEqualTo($exp); }
 
-  public function haveKey($key) { array_key_exists($key, $this->actual); }
+  public function haveKey($key) { $this->check(array_key_exists($key, $this->actual), "have key \"$key\""); }
 
   public function raiseAnException() {
     try {
       call_user_func($this->actual);
     } catch (Exception $e) {
-      $this->check(true);
+      $this->check(true, "raise an exception");
       return;
     }
-    $this->check(false);
+    $this->check(false, "raise an exception");
   }
 
   public function fail() {
     try {
       call_user_func($this->actual);
     } catch (TestFailure $failure) {
-      $this->check(true);
+      $this->check(true, "test fail");
       return;
     }
-    $this->check(false);
+    $this->check(false, "test fail");
   }
 
-  private function check($boolean, $exp = null) {
+  private function check($boolean, $exp) {
     $this->expected = $exp;
     if ($this->negated) { $boolean = !$boolean; }
     if (!$boolean) {
