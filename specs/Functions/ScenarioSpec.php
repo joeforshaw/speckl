925c479c93fdbad1eed62b8c86314b2c580a7c6a
Joe Forshaw <joe@joeforshaw.com>
2021-08-22 18:31:35 +0100

    Scenario spec wasn't running as a scenario woops

--- specs/Functions/ScenarioSpec.php
+++ specs/Functions/ScenarioSpec.php
@@ -1,7 +1,7 @@
 <?php
 
 describe('A scenario block', function() {
-  it('runs like an it block', function() {
+  scenario('runs like an it block', function() {
     expect(true)->to->beTrue();
   });
 });
